fb58e36e7e4c1fd627b665bcfdce525ba0c5e168
Matías Benjamín Moreno Juárez <morenobmatias@gmail.com>
2019-07-26 20:13:28 +0100

    messages
    client created confirmation

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClientesService } from './clientes.service';
+import swal from 'sweetalert2';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-clientes',
@@ -10,11 +12,65 @@ import { ClientesService } from './clientes.service';
 export class ClientesComponent implements OnInit {
 
   listaClientes: Array<Cliente>;
+  paginator: any;
 
-  constructor(private clienteService: ClientesService) { }
+  constructor(private clienteService: ClientesService,
+              private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
-    this.listaClientes = this.clienteService.getClientes();
+
+    this.activatedRoute.paramMap.subscribe(
+      params => {
+        let page: number = +params.get('page');
+
+        if (!page) {
+          page = 0;
+        }
+
+        /* Podria usar un .pipe.tap(this.listaClientes = clientes).subscribe(); */
+        this.clienteService.getClientes(page).subscribe(
+        /* tengo que meter el content json paginado en lista clientes */
+        // (clientes) => this.listaClientes = clientes
+        (response => {
+            this.listaClientes = response.content as Array<Cliente>;
+            this.paginator = response;
+        } ));
+      }
+    );
+  }
+
+  delete(cliente: Cliente): void {
+
+    const swalWithBootstrapButtons = swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger mr-3'
+      },
+      buttonsStyling: false,
+    });
+
+    swalWithBootstrapButtons.fire({
+      title: 'Estás seguro?',
+      text: `¿Seguro que desea eliminar a ${cliente.nombre} ${cliente.apellido}`,
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Sí, bórralo!',
+      cancelButtonText: 'No, cancela!',
+      reverseButtons: true
+    }).then((result) => {
+      if (result.value) {
+        this.clienteService.delete(cliente.id).subscribe(
+          (response) => {
+            this.listaClientes = this.listaClientes.filter((cli) => cli !== cliente);
+            swalWithBootstrapButtons.fire(
+              'Borrado!',
+              `El cliente ${cliente.nombre} ${cliente.apellido} ha sido eliminado.`,
+              'success'
+            );
+          }
+        );
+      }
+    });
   }
 
 }
